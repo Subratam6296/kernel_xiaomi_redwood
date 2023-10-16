Atom-X-Devs/scarlet_xiaomi_sdm660

Makefile: Conditionally use `-polly-reschedule` cmdline option
The -polly-reschedule cmdline option optimizes SCoPs using ISL.

This is only supported by clang greater than version 13.0.0.

Reference: https://github.com/llvm/llvm-project/commit/ced20c6672970ee416147b0fc8f2fb6e733acbc5

Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

Makefile
@@ -694,7 +694,8 @@ POLLY_FLAGS	+= -mllvm -polly \
 		   -mllvm -polly-vectorizer=stripmine
 
 ifeq ($(shell test $(CONFIG_CLANG_VERSION) -gt 130000; echo $$?),0)
-POLLY_FLAGS	+= -mllvm -polly-loopfusion-greedy=1
+POLLY_FLAGS	+= -mllvm -polly-loopfusion-greedy=1 \
+	     -mllvm -polly-reschedule=1
 else
 POLLY_FLAGS	+= -mllvm -polly-opt-fusion=max
 endif

Subratam6296/kernel_xiaomi_redwood

Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

Makefile
@@ -767,7 +767,8 @@ KBUILD_CFLAGS	+= -mllvm -polly \
 		   -mllvm -polly-vectorizer=stripmine
 
 ifeq ($(shell test $(CONFIG_CLANG_VERSION) -gt 130000; echo $$?),0)
-KBUILD_CFLAGS	+= -mllvm -polly-loopfusion-greedy=1
+KBUILD_CFLAGS	+= -mllvm -polly-loopfusion-greedy=1 \
+		   -mllvm -polly-reschedule=1
 else
 KBUILD_CFLAGS	+= -mllvm -polly-opt-fusion=max
 endif